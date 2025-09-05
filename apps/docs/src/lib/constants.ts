298ae8ef400069db30254de08c3cb1cd6a68e21b
Ankur Chauhan <hi@ankurchauhan.design>
2025-09-05 13:04:06 +0200

    fix: pnpm installation cmd mismatch

--- apps/docs/src/lib/constants.ts
+++ apps/docs/src/lib/constants.ts
@@ -1,5 +1,5 @@
 const INSTALL_CMD = [
-  { label: 'pnpm', value: 'pnpm install lisere' },
+  { label: 'pnpm', value: 'pnpm add lisere' },
   { label: 'npm', value: 'npm install lisere' },
   { label: 'yarn', value: 'yarn add lisere' },
 ];
